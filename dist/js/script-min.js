f0fa316a4dd9f59ac231cdca85728062d87c06e5
Kacper <kacperwach96@gmail.com>
2024-04-18 18:02:32 +0200

    Repo init

---
+++ dist/js/script-min.js
@@ -0,0 +1,2 @@
+"use strict";console.log("dziala");
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiYUFBQUEsUUFBUUMsSUFBSSxRQUFRIiwiZmlsZSI6InNjcmlwdC1taW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnZHppYWxhJyk7Il19
